3372f283ba81be9a6bd1e00eddd28ba905d6f856
vasilii-kovalev <vasya.kowalev@yandex.ru>
2018-10-01 00:15:51 +0300

    Change link text in NotFound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-    <Link to="/tasks/all">Not Found</Link>
+    <Link to="/tasks/all">Link to all tasks</Link>
 );
 
 export default NotFound;
